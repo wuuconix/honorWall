e12de5b1edc14a71c19bbe83d597ffa4d7e5079d
wuuconix <1521900139@qq.com>
2022-05-16 16:36:31 +0800

    重制版荣誉墙初始化

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # honorWall
-荣誉墙重制版
+
+武丑兄荣誉墙重置版
+
+这回不再使用任何UI库，包括之前使用过的Fomantic UI库，而完全借助CSS原生的力量，flex布局是真滴好用！
+
+由于设计到大量图片，仍然使用了vue来，利用声明式模板来提高编程效率。
+
+api鉴权使用jwt，没有session-cookie的跨域问题。
+
+在线预览链接: https://url.wuuconix.link/honor
+
+![页面截图](https://tvax4.sinaimg.cn/large/007YVyKcly1h2abyyc5h1j31gs0o1x0p.jpg)
---
+++ src/App.vue
@@ -0,0 +1,222 @@
+<template>
+    <div class="loginout">
+        <a href="https://github.com/wuuconix/honorWall" target="_blank" title="Github仓库">
+            <img src="https://tvax3.sinaimg.cn/large/007YVyKcly1h27w6nxmvqj305k05kaa3.jpg">
+        </a>
+        <span title="自信地没有用任何UI库，手写布局！"> 武丑兄荣誉墙 </span>
+        <button v-if="!online" @click="authenticate = true" class="login" title="登录后可见武丑兄的荣誉整证书!">Login</button>
+        <button v-if="online" @click="logout" class="logout">Logout</button>
+    </div>
+    <div class="imgs_wrapper">
+        <img v-for="img in imgs" :src="img['url']" @click="handle_click(img['url'])">
+    </div>
+    <transition name="fade">
+        <div class="shadow" v-show="preview" @click="preview=false">
+            <img :src="previewSrc" class="preview">
+        </div>
+    </transition>
+    <transition name="fade">
+        <div class="shadow auth" v-show="authenticate" @click="handle_click2" ref="shadow2">
+            <input type="text" placeholder="user" v-model="user">
+            <input type="password" placeholder="pass" v-model="pass">
+            <button @click="login">Login</button>
+            <p>{{ response }}</p>
+        </div>
+    </transition>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            imgs: [],
+            preview: false,
+            previewSrc: "",
+            authenticate: false,
+            user: "",
+            pass: "",
+            online: false,
+            response: ""
+        }
+    },
+    methods: {
+        get_imgs() {
+            fetch("https://wuuconix.link/api/honor/imgs", {
+                headers: {authorization: localStorage.getItem("token")}
+            }).then(res => res.json()).then(res => {
+                if (res.group == "guest") {
+                    this.online = false
+                } else if (res.group == "admin") {
+                    this.online = true
+                }
+                this.imgs = res.imgs
+            })
+        },
+        handle_click(src) {
+            this.previewSrc = src
+            this.preview = true
+        },
+        handle_click2(e) {
+            if (e.target == this.$refs.shadow2) {
+                this.response = ""
+                this.authenticate = false
+            }
+        },
+        login() {
+            fetch("https://wuuconix.link/api/honor/login", {
+                method: "POST",
+                body: new URLSearchParams({user: this.user, pass: this.pass})
+            }).then(res => res.json()).then(res => {
+                if (res.success) {
+                    localStorage.setItem("token", res.token)
+                    this.authenticate = false
+                    this.get_imgs()
+                    this.response = ""
+                } else {
+                    this.response = "账号或密码错误"
+                }
+            })
+        },
+        logout() {
+            localStorage.removeItem("token")
+            this.get_imgs()
+        }
+    },
+    mounted() {
+        document.addEventListener("keypress", (e) => {
+            if (e.key == "Enter") {
+                if (this.authenticate) {
+                    this.login()
+                } else {
+                    this.authenticate = true
+                }
+            }
+        })
+        this.get_imgs()
+    },
+}
+
+</script>
+
+<style>
+
+* {
+    padding: 0;
+    margin: 0;
+}
+div.imgs_wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    height: 90vh;
+    width: 100%;
+}
+
+div.imgs_wrapper img {
+    flex: 30%;
+    height: 40%;
+    object-fit: contain;
+    padding: 30px;
+    filter: drop-shadow(10px 10px 10px rgba(0,0,0,.5));
+}
+
+div.shadow {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0,0,0,.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+}
+
+div.auth {
+    flex-direction: column;
+}
+
+div.shadow img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+.fade-enter-active, .fade-leave-active {
+	transition: opacity .25s
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+	opacity: 0
+}
+
+input {
+    width: 30%;
+    height: 32px;
+    line-height: 32px;
+    font-size: 16px;
+    font-family: Microsoft YaHei;
+    border-radius: 5px;
+    margin: 10px;
+    border: 3px solid #337ecc;
+    outline: none;
+}
+
+div.btn_wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+button {
+    width: 64px;
+    height: 32px;
+    color: white;
+    margin: 10px;
+    background-color: #337ecc;
+    border: 0px;
+    border-radius: 5px;
+    outline: none;
+}
+
+div.loginout {
+    position: sticky;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 10%;
+    background-color: #337ecc;
+    z-index: 99;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 5%;
+    box-sizing:border-box
+}
+
+div.loginout button.login {
+    background-color:  #95d475;
+}
+
+div.loginout button.logout {
+    background-color:  #f89898
+}
+
+div.loginout img {
+    height: 40px;
+    border-radius: 20px;
+}
+
+button:hover {
+    cursor: pointer;
+}
+
+span, p {
+    color: white;
+}
+</style>
